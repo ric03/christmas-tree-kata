04e3fadfeac3c7aaac7ca71360153a6f4a0d1748
Tom Schneider <ric0.github@gmail.com>
2022-11-27 23:21:50 +0100

    feat: add kotest dependencies for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,14 @@ repositories {
     mavenCentral()
 }
 
+val kotestVersion = "5.5.4"
+
 dependencies {
     testImplementation(kotlin("test"))
+    // Kotest
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-framework-datatest:$kotestVersion")
 }
 
 tasks.test {
