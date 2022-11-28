62cfea6995050a64547aac206642ced8f0ce33f7
Tom Schneider <ric0.github@gmail.com>
2022-11-28 23:03:30 +0100

    feat: implement Christmas Tree Kata using java

--- src/main/java/com/github/ric03/christmastreekata/ChristmasTreeKata.java
+++ src/main/java/com/github/ric03/christmastreekata/ChristmasTreeKata.java
@@ -1,8 +1,32 @@
 package com.github.ric03.christmastreekata;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class ChristmasTreeKata {
 
-    public String drawTree() {
-        return null;
+    private static final String STUMP = "|";
+    public static final String LEAF = "X";
+
+    public List<String> drawTree(int size) {
+        if (size <= 0) {
+            return List.of(STUMP);
+        }
+
+        List<String> tree = IntStream
+            .range(0, size)
+            .mapToObj(idx -> {
+                var indentation = " ".repeat(size - idx - 1);
+                var leaves = LEAF.repeat(idx * 2 + 1);
+                return indentation.concat(leaves);
+            })
+            .collect(Collectors.toList());
+
+        var indentation = " ".repeat(size - 1);
+        var stump = indentation.concat(STUMP);
+
+        tree.add(stump);
+        return tree;
     }
 }
--- src/test/java/christmastreekata/ChristmasTreeKataTest.java
+++ src/test/java/christmastreekata/ChristmasTreeKataTest.java
@@ -2,6 +2,9 @@ package christmastreekata;
 
 import com.github.ric03.christmastreekata.ChristmasTreeKata;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,6 +14,76 @@ class ChristmasTreeKataTest {
 
     @Test
     void givenSize_whenDrawTree_thenResultIsNotNull() {
-        assertThat(uut.drawTree()).isNotNull();
+        assertThat(uut.drawTree(1)).isNotNull();
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-99, -1, 0})
+    void givenZeroOrNegativeSize_whenDrawTree_thenReturnOnlyTreeStump(int size) {
+        // Act
+        var result = uut.drawTree(size);
+        // Assert
+        assertThat(result).containsExactly("|");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,X", "2,-X", "3,--X"})
+    void givenZeroOrNegativeSize_whenDrawTree_thenReturnOnlyTreeStump(int size, String expectedIndentation) {
+        // Act
+        var result = uut.drawTree(size);
+        // Assert
+        assertThat(result).containsOnlyOnce(expectedIndentation.replace('-', ' '));
+    }
+
+    @Test
+    void givenSize1_whenDrawTree_thenReturn1RowsOfLeavesAndATreeStump() {
+        // Act
+        var result = uut.drawTree(1);
+        // Assert
+        assertThat(result).containsExactly(
+            "X",
+            "|"
+        );
+    }
+
+    @Test
+    void givenSize2_whenDrawTree_thenReturn2RowsOfLeavesAndATreeStump() {
+        // Act
+        var result = uut.drawTree(2);
+        // Assert
+        assertThat(result).containsExactly(
+            " X",
+            "XXX",
+            " |"
+        );
+    }
+
+    @Test
+    void givenSize3_whenDrawTree_thenReturn3RowsOfLeavesAndATreeStump() {
+        // Act
+        var result = uut.drawTree(3);
+        // Assert
+        assertThat(result).containsExactly(
+            "  X",
+            " XXX",
+            "XXXXX",
+            "  |"
+        );
+    }
+
+    @Test
+    void givenSize5_whenDrawTree_thenReturn5RowsOfLeavesAndATreeStump() {
+        // Act
+        var result = uut.drawTree(5);
+        // Assert
+        assertThat(result).containsExactly(
+            "    X",
+            "   XXX",
+            "  XXXXX",
+            " XXXXXXX",
+            "XXXXXXXXX",
+            "    |"
+        );
+    }
+
 }
